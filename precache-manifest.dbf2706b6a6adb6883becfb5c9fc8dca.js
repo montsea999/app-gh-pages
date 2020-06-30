672e8396d6b8ac96ed0a682095c22e2f6ff33a67
Montserrat <montsea999@gmail.com>
2020-07-01 01:21:04 +0200

    Updates

---
+++ precache-manifest.dbf2706b6a6adb6883becfb5c9fc8dca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ad8a7bb7b63e87af08a00f9c9298c4",
+    "url": "/app-gh-pages/index.html"
+  },
+  {
+    "revision": "c405dad29ab9bc80fb51",
+    "url": "/app-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc3a96da40dc66ee0a96",
+    "url": "/app-gh-pages/static/js/2.3b2dfc5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-gh-pages/static/js/2.3b2dfc5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c405dad29ab9bc80fb51",
+    "url": "/app-gh-pages/static/js/main.1e4f8ff3.chunk.js"
+  },
+  {
+    "revision": "ae361a7d02534c622397",
+    "url": "/app-gh-pages/static/js/runtime-main.dd220d67.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
